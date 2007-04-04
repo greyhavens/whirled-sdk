dc81170371ad4fbd9e211ac2b4bf35735c6af0ed
mdb <mdb@localhost>
2007-04-04 18:38:45 +0000

    The start of the beginning of the "public" Whirled repository wherein the standalone test server lives and will live all of the source to our public (avatar, pet, game, etc.) projects.

---
+++ src/java/com/whirled/server/WhirledServer.java
@@ -0,0 +1,13 @@
+//
+// $Id$
+
+package com.whirled.server;
+
+import com.threerings.toybox.server.ToyBoxServer;
+
+/**
+ * Handles setting up the Whirled standalone test server.
+ */
+public class WhirledServer extends ToyBoxServer
+{
+}
