841dfb8a7cc3dbbc297672d8c0118a1962623547
mdb <mdb@localhost>
2007-10-02 21:07:18 +0000

    We need this and probably some other bits in a bit.

--- src/java/com/whirled/data/WhirledGame.java
+++ src/java/com/whirled/data/WhirledGame.java
@@ -13,4 +13,9 @@ public interface WhirledGame
 {
     /** A message dispatched to each player's client object when flow is awarded. */
     public static final String FLOW_AWARDED_MESSAGE = "FlowAwarded";
+
+    /**
+     * Configures this Whirled game with its service.
+     */
+    public void setWhirledGameService (WhirledGameMarshaller value);
 }
