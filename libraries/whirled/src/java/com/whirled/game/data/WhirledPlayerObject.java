4e4be627a2ec818c1f1454030862170beea547ba
jamie <jamie@localhost>
2008-08-15 20:39:48 +0000

    Create the correct kind of occupant info for whirled players

--- libraries/whirled/src/java/com/whirled/game/data/WhirledPlayerObject.java
+++ libraries/whirled/src/java/com/whirled/game/data/WhirledPlayerObject.java
@@ -4,6 +4,8 @@
 package com.whirled.game.data;
 
 import com.threerings.crowd.data.BodyObject;
+import com.threerings.crowd.data.OccupantInfo;
+import com.threerings.crowd.data.PlaceObject;
 
 /**
  * Body for playing whirled games.
@@ -11,4 +13,13 @@ import com.threerings.crowd.data.BodyObject;
  */
 public class WhirledPlayerObject extends BodyObject
 {
+    @Override public OccupantInfo createOccupantInfo (PlaceObject plObj)
+    {
+        if (plObj instanceof WhirledGameObject) {
+            return new WhirledGameOccupantInfo(this);
+
+        } else {
+            return super.createOccupantInfo(plObj);
+        }
+    }
 }
